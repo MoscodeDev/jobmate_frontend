c973a30a4efad7eebb3226edd35a124d73ccac59
Musah-K <elikarany@gmail.com>
2025-08-11 08:53:51 +0300

    Build for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
 
 import { Provider } from 'react-redux'
 import {store} from './app/store.js'
@@ -10,9 +9,7 @@ import {store} from './app/store.js'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/jobmate_frontend">
         <App />
-      </BrowserRouter>
     </Provider>
   </StrictMode>,
 )
